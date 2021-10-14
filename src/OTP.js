b1778f5a066b3fa577e831389ab8655539f28ad0
Tyson Chuang <tyson.chuang@ehsn.com.tw>
2021-10-14 11:14:51 +0800

    refs #29652 FIX npm install error

--- src/OTP.js
+++ src/OTP.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 function OTP({ isWebAPI = false }) {
     const [code, setCode] = useState()
     useEffect(() => {
+        alert("OTPCredential' in window'", 'OTPCredential' in window)
         if (isWebAPI && 'OTPCredential' in window) {
             window.addEventListener('DOMContentLoaded', e => {
                 const input = document.querySelector('input[autocomplete="one-time-code"]')
@@ -13,6 +14,7 @@ function OTP({ isWebAPI = false }) {
                         ac.abort()
                     })
                 }
+                alert('navigator.credentials:', navigator.credentials)
                 navigator.credentials
                     .get({
                         otp: { transport: ['sms'] },
@@ -20,15 +22,23 @@ function OTP({ isWebAPI = false }) {
                     })
                     .then(otp => {
                         setCode(otp.code)
-                        alert('SMS Code:', otp.code)
+                        alert('SMS Code:', otp, otp.code)
                         if (form) form.submit()
                     })
                     .catch(err => {
+                        alert('cache err:', err)
                         console.log(err)
                     })
             })
         }
     }, [isWebAPI])
-    return <input type="text" autoComplete="one-time-code" inputMode="numeric" value={code} />
+    return <div>
+        <h1>{isWebAPI ? "isWebAPI" : "NOT WebAPI"}</h1>
+        <input type="text" autoComplete="one-time-code" inputMode="numeric" value={code} />
+        <div onClick={() => {
+            alert('navigator.credentials: ', navigator.credentials)
+            console.log('navigator.credentials: ', navigator.credentials)
+        }}>TEST credentials</div>
+    </div>
 }
 export default OTP
